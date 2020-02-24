e5bc0281d5f0668f2c161fe88bf105f6bfacce63
João Victor <jvsouzax@gmail.com>
2020-02-24 10:11:38 -0300

    add: campaign mailer tests

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,13 +1,31 @@
-class CampaignMailer < ApplicationMailer
+require "rails_helper"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.campaign_mailer.raffle.subject
-  #
-  def raffle
-    @greeting = "Hi"
+RSpec.describe CampaignMailer, type: :mailer do
+  describe "raffle" do
 
-    mail to: "to@example.org"
+    before do
+      @campaign = create(:campaign)
+      @member   = create(:member, campaign: @campaign)
+      @friend = create(:member, campaign: @campaign)
+      @mail = CampaignMailer.raffle(@campaign, @member, @friend)
+    end
+
+
+    it "renders the headers" do
+      expect(@mail.subject).to eq("Nosso Amigo Secreto: #{@campaign.title}")
+      expect(@mail.to).to eq([@member.email])
+    end
+
+    it "body have member name" do
+      expect(@mail.body.encoded).to match(@member.name)
+    end
+
+    it "body have campaign creator name" do
+      expect(@mail.body.encoded).to match(@campaign.user.name)
+    end
+
+    it "body have member link to set open" do
+      expect(@mail.body.encoded).to match("/members/#{@member.token}/opened")
+    end
   end
 end
